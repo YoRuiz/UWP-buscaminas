1592a33872bf5468d6c221e9833734901e002c8d
José Antonio Ruiz Jiménez <jarjcfgs@gmail.com>
2017-02-07 12:51:21 +0100

    Commit con modelo creado, delegateCommand y VMBase

---
+++ buscaminas/buscaminas/Models/casilla.cs
@@ -0,0 +1,48 @@
+﻿using buscaminas.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buscaminas.Models
+{
+    public class casilla : clsVMBase
+    {
+        private int _valor;
+        private Uri _imagen;
+
+        public casilla()
+        {
+
+        }
+        public casilla(int valor, Uri imagen)
+        {
+            this._valor = valor;
+            this._imagen = imagen;
+        }
+
+        public int valor
+        {
+            get
+            {
+                return _valor;
+            }
+            set
+            {
+                _valor = value;
+            }
+        }
+        public Uri imagen
+        {
+            get{
+                return _imagen;
+            }
+            set{
+                _imagen = value;
+                NotifyPropertyChanged("imagen");
+            }
+        }
+
+    }
+}
